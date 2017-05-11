b8ed8fb520c7ff701edc30f2389f41a243e328e2
lillaorr <lilla.orr@yale.edu>
2017-05-11 16:27:37 -0400

    change full.results to res (Jay's suggestion)
    also fixes some comments and adds specificity two the quota and surplus method error messages

--- R/stv.R
+++ R/stv.R
@@ -1,8 +1,8 @@
 stv <- function(x, seats = 1, file = "", surplusMethod = "Cambridge", quotaMethod = "Droop") {
 
   # Trap wrong names Cambridge and Droop
-  if(!surplusMethod == "Cambridge") stop("Please set surplusMethod = Cambridge") # Currently support
-  if(!quotaMethod == "Droop") stop("Please set quotaMethod = Droop")
+  if(!surplusMethod == "Cambridge") stop("Please set surplusMethod = Cambridge. This is the only method currently supported.")
+  if(!quotaMethod == "Droop") stop("Please set quotaMethod = Droop. This is the only method currently supported.")
 
   junk <- validateBallots(x)
 
@@ -11,48 +11,47 @@ stv <- function(x, seats = 1, file = "", surplusMethod = "Cambridge", quotaMetho
   # Initialize various things:
   elim <- c()                                  # Store names of eliminated candidates
   elect <- c()                                 # Store names of elected candidates
-  included.ballots <- rep(TRUE, nrow(x))      # Ballots excluded by surplus redistribution (Invert this to included.ballots)
-  # names(excluded.ballots) <- rownames(x)       # Will use names during surplus allocation
+  included.ballots <- rep(TRUE, nrow(x))       # Ballots included with surplus redistribution
   unfilled <- seats                            # Vacant seats at a given round
   Nround <- 0                                  # Current number of round
 
-  full.results <- data.frame(matrix(NA, ncol = 9 + ncol(x), nrow = 0)) # rename as res
-  names(full.results) <- c("ballots", "seats.to.fill", "quota", "max.vote.count", "min.vote.count",
+  res <- data.frame(matrix(NA, ncol = 9 + ncol(x), nrow = 0))
+  names(res) <- c("ballots", "seats.to.fill", "quota", "max.vote.count", "min.vote.count",
                            "elim.cand", "tied.for.elim", "elect.cand", "surplus", names(x))
 
   # Iteratively elect/eliminate candidates till all seats are filled:
   while (unfilled > 0) {
     Nround <- Nround + 1
-    full.results[Nround, ] <- rep(NA, ncol(full.results))
-    full.results$seats.to.fill[Nround] <- unfilled
+    res[Nround, ] <- rep(NA, ncol(res))
+    res$seats.to.fill[Nround] <- unfilled
     curr.candidates <- setdiff(names(x), c(elim, elect))
 
     # Check if #remaining candidates already equal #unfilled seats:
     #--- CODING CHECK ---: In validateBallots() add the case where after cleaning, valid ncols < seats (i.e. will have unfilled seats)
     if (unfilled == length(curr.candidates)) {
       elect <- c(elect, curr.candidates)
-      full.results$elect.cand[Nround] <- paste(curr.candidates, collapse = "; ")
-      full.results$seats.to.fill[Nround] <- 0
-      full.results[Nround, elect] <- "Elected"
-      full.results[Nround, elim] <- "Eliminated"
-      full.results$quota[Nround] <- "Won by elim"
+      res$elect.cand[Nround] <- paste(curr.candidates, collapse = "; ")
+      res$seats.to.fill[Nround] <- 0
+      res[Nround, elect] <- "Elected"
+      res[Nround, elim] <- "Eliminated"
+      res$quota[Nround] <- "Won by elim"
 
       if (file != "") {
         if (substr(file, nchar(file)-3, nchar(file)) != ".csv") warning("File name provided does not include .csv extention")
-        write.table(full.results, file = file, sep = ",", row.names = FALSE)
+        write.table(res, file = file, sep = ",", row.names = FALSE)
       }
 
-      return(list("elected" = elect, "detailed.info" = full.results))
+      return(list("elected" = elect, "detailed.info" = res))
     }
 
     # For remaining candidates, get valid ballots (1. not with all NA entries 2. excluded by surplus)
     curr.ballots <- (rowSums(!is.na(x[ ,curr.candidates])) > 0) & included.ballots
     ballot.size <- sum(curr.ballots)
-    full.results$ballots[Nround] <- ballot.size
+    res$ballots[Nround] <- ballot.size
 
     # Calculate Quota: Manually add 1 instead of using "ceiling()" to address whole numbers
     if (quotaMethod == "Droop") quota <- floor(ballot.size/(unfilled + 1)) + 1
-    full.results$quota[Nround] <- quota
+    res$quota[Nround] <- quota
 
     # Get top choice for each valid ballot then tabulate it (i.e. get vote count for each candidate):
     # ---NOTE--- which.min() function gives first position with minimum value
@@ -60,9 +59,9 @@ stv <- function(x, seats = 1, file = "", surplusMethod = "Cambridge", quotaMetho
     top.choice[!curr.ballots] <- NA
     vote.counts <- table(factor(top.choice, levels = curr.candidates)) # try: table(top.choice)
 
-    full.results[Nround, names(vote.counts)] <- vote.counts
-    full.results$max.vote.count[Nround] <- max(vote.counts)
-    full.results$min.vote.count[Nround] <- min(vote.counts)
+    res[Nround, names(vote.counts)] <- vote.counts
+    res$max.vote.count[Nround] <- max(vote.counts)
+    res$min.vote.count[Nround] <- min(vote.counts)
     full.results[Nround, elect] <- "Elected"
     full.results[Nround, elim] <- "Eliminated"
 
